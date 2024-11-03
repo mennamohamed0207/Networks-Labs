357eda5a00733b2adf513f7d4d663a41d22ae8ee
mennamohamed0207 <menamohamed0207@gmail.com>
2024-11-03 11:44:35 +0200

    Adding the base of lab 5

---
+++ Lab5/sender.cc
@@ -0,0 +1,64 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "sender.h"
+
+Define_Module(Sender);
+
+void Sender::initialize()
+{
+    // TODO - Generated method body
+    EV<<"Sender Intialize"<<endl;
+    scheduleAt(simTime(),new cMessage(""));
+
+}
+
+void Sender::handleMessage(cMessage *msg)
+{
+    // TODO - Generated method body
+    MyMessage_Base* newMessage=new MyMessage_Base("Name");
+    std::string input;
+    std::cout<<"Enter a message:(if you entered 'end' then it will end simulation) "<<endl;
+        EV<<"Enter a message:(if you entered 'end' then it will end simulation)"<<endl;
+
+        std::cin>>input;
+        std::cout<<"You Entered : "<<input<<endl;
+
+
+    EV<<"Message "<<input<<" Before Modification :"<<endl;
+    std::bitset<8>charCount(input.size());
+    EV<<charCount<<endl;
+    newMessage->setM_Header((char)charCount.to_ulong());
+    newMessage->setM_Type(0);
+    int size=0;
+    std::bitset<8>parity(0);
+    parity=parity^charCount;
+    newMessage->setM_PayloadArraySize(input.size()+1);
+
+    for (int i=0;i<input.size();i++)
+    {
+        size++;
+        std::bitset<8>character(input[i]);
+        EV<<character<<endl;
+        newMessage->setM_Payload(size,input[i]);
+        parity=parity^character;
+
+    }
+    EV<<parity<<endl;
+    newMessage->setM_Trailer((char)parity.to_ulong());
+    send(newMessage,"sender_out");
+
+
+}
